2b9c946b5551b5955137309984d3f5d24d334616
Kubinic <Osiptsov84@gmail.com>
2023-02-15 10:38:35 +0700

    Чётные числа в составе числа N

---
+++ even_in_1_on_N/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число : ");
+int N = int.Parse(Console.ReadLine()!);
+int number = 1;
+int count = 1;
+while (count <= N ){
+    if(number % 2 == 0) 
+    Console.Write(number + " ");
+    number ++;
+    count ++;
+}
